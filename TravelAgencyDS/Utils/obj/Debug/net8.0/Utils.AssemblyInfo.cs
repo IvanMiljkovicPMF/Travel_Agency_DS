259dc8e393ef7ca25b074897f18ed24848ed52a7
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2025-08-21 23:45:01 +0200

    new

--- TravelAgencyDS/Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
+++ TravelAgencyDS/Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414809e3d23e2dd303b1e43ca7918b3a9853e48a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be81c56804b8cd2790a440814c04c27c364fd351")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
